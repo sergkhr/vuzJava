94534311945f6c7f82233e855a4630aa66b148a1
sergkhr <serg.khrechko@gmail.com>
2022-11-26 18:16:04 +0300

    did 28

---
+++ src/Prac28/MyFolderSaver.java
@@ -0,0 +1,22 @@
+package Prac28;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class MyFolderSaver {
+	static Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("enter the path to the folder");
+		String path = in.nextLine();
+		File folder = new File(path);
+		LinkedList<String> list = new LinkedList<String>();
+		for (File file : folder.listFiles()) {
+			list.add(file.getName());
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println(list.get(i));
+		}
+	}
+	//path starts at vuzJava by default
+}
